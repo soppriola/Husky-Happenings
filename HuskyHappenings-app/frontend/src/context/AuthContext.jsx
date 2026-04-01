be226353026230b03e5fd1c1d35735958a1ba582
TheHylianLegend <ohp2004@gmail.com>
2026-04-01 12:56:37 -0400

    Implemented ProtectedRoute component and AuthContext context

--- HuskyHappenings-app/frontend/src/context/AuthContext.jsx
+++ HuskyHappenings-app/frontend/src/context/AuthContext.jsx
@@ -1,2 +1,48 @@
 import { createContext, useContext, useState, useEffect } from "react";
 
+const AuthContext = createContext(null);
+
+export function useAuth(){
+  return useContext(AuthContext);
+}
+
+// Handles authentication of users session
+export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    async function checkSession() {
+      try {
+        // CHANGE TO HTTPS LATER
+        const response = await fetch("http://localhost:5000/api/me", {
+          method: "GET",
+          credentials: "include",
+       });
+
+        setIsAuthenticated(response.ok);
+      } catch {
+        setIsAuthenticated(false);
+      }
+      setLoading(false);
+    }
+
+    checkSession();
+  },
+[]);
+
+  const login = async () => {
+    setIsAuthenticated(true);
+  };
+
+  // CHANGE TO HTTPS LATER
+  const logout = async () => {
+    await fetch("http://localhost:5000/api/logout", {
+        method: "POST",
+        credentials: "include",
+    });
+    setIsAuthenticated(false);
+  };
+
+  return (<AuthContext.Provider  value={{ loading, isAuthenticated, login, logout, }}> {children} </AuthContext.Provider>);
+}
