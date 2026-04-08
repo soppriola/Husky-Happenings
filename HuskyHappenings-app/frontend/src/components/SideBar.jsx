58aa4dfe6fa5044ecf0603b92c86430da9f6b625
TheHylianLegend <ohp2004@gmail.com>
2026-04-08 14:00:14 -0400

    Implemented states for sidebar based on login state

--- HuskyHappenings-app/frontend/src/components/SideBar.jsx
+++ HuskyHappenings-app/frontend/src/components/SideBar.jsx
@@ -3,17 +3,27 @@ import { useAuth } from "../context/AuthContext";
 import "./Sidebar.css";
 
 export default function Sidebar() {
+  const { isAuthenticated, logout } = useAuth();
+
   return (
     <aside className="sidebar">
       <h2 className="sidebar-title">HuskyHappenings</h2>
 
       <nav className="sidebar-nav">
         <Link to="/">Home</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/login">Log In</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/settings">Settings</Link>
-        <Link to="/messages">Messages</Link>
+        {isAuthenticated ? (
+          <>
+            <button onClick={logout}>Logout</button>
+            <Link to="/profile">Profile</Link>
+            <Link to="/settings">Settings</Link>
+            <Link to="/messages">Messages</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Log In</Link>
+          </>
+        )}
       </nav>
     </aside>
   );
